6de30c5c5ab2483193e1e1b6738c13ecef4de6a4
SeanKervick <20104300@mail.wit.ie>
2024-06-12 15:10:49 +0100

    Exercise - Show favourite icon on Movie Details page..

--- src/components/headerMovie/index.tsx
+++ src/components/headerMovie/index.tsx
@@ -5,7 +5,10 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
-import { MovieDetailsProps } from "../../types/interfaces"; 
+import { MovieDetailsProps } from "../../types/interfaces";
+import Avatar from "@mui/material/Avatar";
+import FavoriteIcon from '@mui/icons-material/Favorite'; 
+
 
 const styles = {
     root: {  
@@ -15,15 +18,30 @@ const styles = {
     flexWrap: "wrap",
     padding: 1.5,
   },
+  avatar: {
+    backgroundColor: "rgb(255, 0, 0)",
+  },
 };
 
 const MovieHeader: React.FC<MovieDetailsProps> = (movie) => {
+
+  // retrieve favorite movies from local storage
+  const faveouriteMovies = JSON.parse(localStorage.getItem("favourites") || '[]'); // from favouriteMoviesPage
+  const isFavouriteMovie = faveouriteMovies.find((faveouriteMovies: { id: number; }) => faveouriteMovies.id === movie.id); // check if the movie's id is in the list of favourite movies
   
   return (
     <Paper component="div" sx={styles.root}>
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
+      {
+        // check if the movie is a favourite, display the icon if true
+         isFavouriteMovie ? ( // from index.tsx
+            <Avatar sx={styles.avatar}>
+              <FavoriteIcon />
+            </Avatar>
+          ) : null
+        }
 
       <Typography variant="h4" component="h3">
         {movie.title}{"   "}
@@ -36,6 +54,7 @@ const MovieHeader: React.FC<MovieDetailsProps> = (movie) => {
       <IconButton aria-label="go forward">
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
+
     </Paper>
   );
 };
