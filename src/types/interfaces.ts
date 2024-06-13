76e59a8b0d80967ca015762dfd3e9d3d0a7229b9
SeanKervick <20104300@mail.wit.ie>
2024-06-13 09:56:47 +0100

    Show movie review excerpts; Refactor Details page.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -58,3 +58,9 @@ export interface BaseMovieProps {
   export interface MovieListPageTemplateProps extends BaseMovieListProps {
     title: string;
   }
+
+  export interface Review{
+    id: string;
+    content: string
+    author: string
+  }
